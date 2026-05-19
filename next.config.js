7dad75d8f6f45f751a0c8720ff88acfcdbaa6003
Jawad Ahmad <khanjd265@gmail.com>
2026-05-19 16:12:40 +0500

    fixed config file for github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/cortexure",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true
 };
 
